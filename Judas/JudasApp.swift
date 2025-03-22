3fc8f199dc3cada5e15894a47d036ed3b1ce3a2b
ZacharyZcR <payasonorahc@protonmail.com>
2025-03-22 23:44:50 +0800

    Initial Commit

---
+++ Judas/JudasApp.swift
@@ -0,0 +1,17 @@
+//
+//  JudasApp.swift
+//  Judas
+//
+//  Created by 札克利 on 2025/3/22.
+//
+
+import SwiftUI
+
+@main
+struct JudasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JudasTests/JudasTests.swift
@@ -0,0 +1,17 @@
+//
+//  JudasTests.swift
+//  JudasTests
+//
+//  Created by 札克利 on 2025/3/22.
+//
+
+import Testing
+@testable import Judas
+
+struct JudasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
